7d09c89c9c335ed954ffc93fe805f0dbdc2f812a
Mikhail <m.s1999@mail.ru>
2023-03-14 20:05:45 +0300

    Добавлена цель + жук с прикрепленным сенсором

--- robot.py
+++ robot.py
@@ -1,3 +1,4 @@
+import math
 import time
 import numpy as np
 from zmqRemoteApi import RemoteAPIClient
@@ -13,36 +14,29 @@ class Robot:
         self.name = robot_name
         self.sim = sim
 
+        self.bug = sim.getObject('/PioneerP3DX')
+        self.scanner = sim.getObject('/PioneerP3DX/LaserScanner2D')
+        self.goal = sim.getObject('/Goal')
         self.sensor = Sensor()
 
-    def _set_movement(self, forward_back_vel, left_right_vel, rotation_vel):
-        """
-        Установка движения робота
+    def _set_movement(self, left, right):
+        motorLeft = sim.getObject("./leftMotor")
+        motorRight = sim.getObject("./rightMotor")
+        self.sim.setJointTargetVelocity(motorLeft, left)
+        self.sim.setJointTargetVelocity(motorRight, right)
 
-        Parameters
-        ----------
-        forward_back_vel : float
-            Скорость в направлении вперёд-назад
-        left_right_vel : float
-            Скорость в направлении влево-вправо
-        rotation_vel : float
-            Скорость поворота
-        """
-        wheel_joints = [self.sim.getObject('/' + self.name + '/rollingJoint_fl'),
-                        self.sim.getObject('/' + self.name + '/rollingJoint_rl'),
-                        self.sim.getObject('/' + self.name + '/rollingJoint_rr'),
-                        self.sim.getObject('/' + self.name + '/rollingJoint_fr')]
-        self.sim.setJointTargetVelocity(wheel_joints[0], -forward_back_vel - left_right_vel - rotation_vel)
-        self.sim.setJointTargetVelocity(wheel_joints[1], -forward_back_vel + left_right_vel - rotation_vel)
-        self.sim.setJointTargetVelocity(wheel_joints[2], -forward_back_vel - left_right_vel + rotation_vel)
-        self.sim.setJointTargetVelocity(wheel_joints[3], -forward_back_vel + left_right_vel + rotation_vel)
+    def rotate(self):
+        motorLeft = sim.getObject("./leftMotor")
+        motorRight = sim.getObject("./rightMotor")
+        self.sim.setJointTargetVelocity(motorLeft, 0)
+        self.sim.setJointTargetVelocity(motorRight, 0)
 
     def start(self):
         """
         Старт работы робота
         """
         while 1:
-            self._set_movement(ROBOT_SPEED, 0, 0)
+            self._set_movement(2, 2)
 
             states = [self.sensor.left_sector.status, self.sensor.mid_sector.status, self.sensor.right_sector.status]
             print(states)
@@ -54,11 +48,11 @@ class Robot:
                 # Тут чёткая наводка на цель
                 ...
 
-            distance = self.sensor.min_dist
-            print(distance)
-            if (not np.isnan(distance)) and (distance < ROBOT_STOP_DISTANCE):
-                self._set_movement(0, 0, 0)
-                break
+            # distance = self.sensor.min_dist
+            # print(distance)
+            # if (not np.isnan(distance)) and (distance < ROBOT_STOP_DISTANCE):
+                # self._set_movement(0, 0, 0)
+                # break
 
 
 if __name__ == "__main__":
@@ -66,5 +60,7 @@ if __name__ == "__main__":
     sim = client.getObject('sim')
     sim.startSimulation()
 
-    robot = Robot(sim, 'youBot')
-    robot.start()
+    robot = Robot(sim, 'PioneerP3DX')
+    robot.rotate()
+    # robot.start()
+
